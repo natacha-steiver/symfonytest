1b7fbf5888973451f685a6effd9e9f148739e53e
natacha steiver <natachasteiver@gmail.com>
2023-05-17 14:54:40 +0200

    modify ajax url

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -44,21 +44,25 @@ function logJSONData(e) {
      var offset;
      urlParams.get('offset')?offset=parseInt(urlParams.get('offset')):offset=4;
  
-  
+    if(window.location.href.indexOf("localhost") != -1){
+      var url="http://localhost:8000";
+    }else{
+      var url="http://51.68.227.78/symfonytest/public";
+    }
      //console.log(offset);
      if(offset!=4){
        
          offset=4;
          
-         document.getElementById("more").setAttribute("href", "http://localhost:8000/#more");
-         window.location="http://localhost:8000/#more";
+         document.getElementById("more").setAttribute("href", url+"/#more");
+         window.location=url+"/#more";
         
      
      }else if(offset !=1){
        offset=parseInt(offset)-3;
      
-     document.getElementById("more").setAttribute("href", "http://localhost:8000?limit=limit&offset="+offset+"/#more");
-     window.location="http://localhost:8000?limit=limit&offset="+offset+"/#more";
+     document.getElementById("more").setAttribute("href", url+"?limit=limit&offset="+offset+"/#more");
+     window.location=url+"?limit=limit&offset="+offset+"/#more";
     }
      
      
